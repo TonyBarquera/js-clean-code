ce698902a769f677a981692fcfefb562b4af97cd
Tony Barquera <barqueratony@gmail.com>
2024-02-11 20:28:38 -0600

    arrow functions

---
+++ arrow-functions.js
@@ -0,0 +1,12 @@
+// Arrow Function Definition
+const getResult = () => "Results";
+console.log(getResult());
+
+// Example: 
+// Using function declaration
+[1, 2, 3].map(function(n) {
+  return n * 2;
+});
+
+// Using arrow function
+[1, 2, 3].map(n => n * 2);
